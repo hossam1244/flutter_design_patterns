ba9de80aae98072fa55aef8cf011ec377d0fc2d7
Hossam Rakha <hossamrakha0@gmail.com>
2023-08-23 12:35:09 +0300

    Created bottom navigation

---
+++ lib/behaviour_patterns.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BehaviourPatterns extends StatelessWidget {
+  const BehaviourPatterns({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
